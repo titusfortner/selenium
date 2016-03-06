16a8d71e8af63f5b91915dd147c3330482bf473a
Titus Fortner <titus.fortner@gmail.com>
2016-03-06 01:56:23 -0600

    DO NOT MERGE - use guards

--- rb/spec/integration/selenium/webdriver/spec_support/helpers.rb
+++ rb/spec/integration/selenium/webdriver/spec_support/helpers.rb
@@ -30,10 +30,6 @@ module Selenium
           GlobalTestEnv.reset_driver!(time)
         end
 
-        def quit_driver!
-          GlobalTestEnv.quit_driver
-        end
-
         def ensure_single_window
           GlobalTestEnv.ensure_single_window
         end
