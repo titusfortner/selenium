83b3190905960ad5250d5b173c6c438125907fee
titusfortner <titusfortner@gmail.com>
2022-05-05 23:34:09 -0500

    WIP - toggle separators for Windows

--- third_party/dotnet/devtools/src/generator/CodeGen/CodeGenerationDefinitionTemplateSettings.cs
+++ third_party/dotnet/devtools/src/generator/CodeGen/CodeGenerationDefinitionTemplateSettings.cs
@@ -13,36 +13,36 @@
             DomainTemplate = new CodeGenerationTemplateSettings
             {
                 TemplatePath = "domain.hbs",
-                OutputPath = "{{domainName}}\\{{className}}Adapter.cs",
+                OutputPath = "{{domainName}}{{separator}}{{className}}Adapter.cs",
             };
 
             CommandTemplate = new CodeGenerationTemplateSettings {
                 TemplatePath = "command.hbs",
-                OutputPath = "{{domainName}}\\{{className}}Command.cs",
+                OutputPath = "{{domainName}}{{separator}}{{className}}Command.cs",
             };
 
             EventTemplate = new CodeGenerationTemplateSettings
             {
                 TemplatePath = "event.hbs",
-                OutputPath = "{{domainName}}\\{{className}}EventArgs.cs",
+                OutputPath = "{{domainName}}{{separator}}{{className}}EventArgs.cs",
             };
 
             TypeObjectTemplate = new CodeGenerationTemplateSettings
             {
                 TemplatePath = "type-object.hbs",
-                OutputPath = "{{domainName}}\\{{className}}.cs",
+                OutputPath = "{{domainName}}{{separator}}{{className}}.cs",
             };
 
             TypeHashTemplate = new CodeGenerationTemplateSettings
             {
                 TemplatePath = "type-hash.hbs",
-                OutputPath = "{{domainName}}\\{{className}}.cs",
+                OutputPath = "{{domainName}}{{separator}}{{className}}.cs",
             };
 
             TypeEnumTemplate = new CodeGenerationTemplateSettings
             {
                 TemplatePath = "type-enum.hbs",
-                OutputPath = "{{domainName}}\\{{className}}.cs",
+                OutputPath = "{{domainName}}{{separator}}{{className}}.cs",
             };
         }
 
--- third_party/dotnet/devtools/src/generator/CodeGen/Utility.cs
+++ third_party/dotnet/devtools/src/generator/CodeGen/Utility.cs
@@ -4,6 +4,7 @@ namespace OpenQA.Selenium.DevToolsGenerator.CodeGen
     using System;
     using System.Collections.Generic;
     using System.Text.RegularExpressions;
+    using System.Runtime.InteropServices;
 
     /// <summary>
     /// Contains various utility methods.
@@ -20,10 +21,13 @@ namespace OpenQA.Selenium.DevToolsGenerator.CodeGen
         /// <returns></returns>
         public static string ReplaceTokensInPath(string path, string className, CodeGeneratorContext context, CodeGenerationSettings settings)
         {
+            string separator = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "\\" : "/";
+
             path = path.Replace("{{className}}", className);
             path = path.Replace("{{rootNamespace}}", settings.RootNamespace);
             path = path.Replace("{{templatePath}}", settings.TemplatesPath);
             path = path.Replace("{{domainName}}", context.Domain.Name);
+            path = path.Replace("{{separator}}", separator);
             return path;
         }
 
