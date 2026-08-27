a7c7b9caf56d189a868b0fa54b0a39ce5b907614
Titus Fortner <titus.fortner@gmail.com>
2026-08-27 14:37:06 -0500

    [rb] let a new Windows session settle before the first navigation

--- rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
+++ rb/spec/integration/selenium/webdriver/spec_support/test_environment.rb
@@ -294,9 +294,8 @@ module Selenium
           http_client ||= Remote::Http::Default.new(read_timeout: 30)
           @safari_pairing_attempts ||= 0
 
-          method = :"#{driver}_driver"
           opts = {options: build_options(**), listener: listener, http_client: http_client, service: service}.compact
-          instance = private_methods.include?(method) ? send(method, **opts) : WebDriver::Driver.for(driver, **opts)
+          instance = new_driver_instance(**opts)
           @safari_pairing_attempts = 0
           @create_driver_error_count -= 1 unless @create_driver_error_count.zero?
           if block
@@ -316,6 +315,17 @@ module Selenium
         end
 
         private
+
+        def new_driver_instance(**)
+          method = :"#{driver}_driver"
+          instance = private_methods.include?(method) ? send(method, **) : WebDriver::Driver.for(driver, **)
+          # A new Windows session answers commands before it can navigate: the first navigation is
+          # silently abandoned roughly 6% of the time, landing on chromedriver's start page while the
+          # command still reports success.
+          sleep 1 if Platform.windows?
+
+          instance
+        end
 
         def build_options(**)
           options_method = :"#{browser}_options"
