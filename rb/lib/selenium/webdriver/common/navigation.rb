49447f80c186ae2b245f43743a2703690e33cf96
Titus Fortner <titus.fortner@gmail.com>
2026-08-27 11:32:36 -0500

    [build] separate a slow navigation from one that never happened

--- rb/lib/selenium/webdriver/common/navigation.rb
+++ rb/lib/selenium/webdriver/common/navigation.rb
@@ -30,7 +30,14 @@ module Selenium
 
       def to(url)
         @bridge.get url
-        WebDriver.logger.warn("[nav] requested=#{url} landed=#{@bridge.url}", id: :nav)
+        landed = @bridge.url
+        WebDriver.logger.warn("[nav] requested=#{url} landed=#{landed}", id: :nav)
+        return if landed == url
+
+        5.times do |i|
+          sleep 1
+          WebDriver.logger.warn("[nav-wait] +#{i + 1}s landed=#{@bridge.url}", id: :nav)
+        end
       end
 
       #
